f635f4b327792ce55bd3145598606ca565865003
abdelhai <abdelhaijamal95@gmail.com>
2023-12-05 13:58:15 +0200

    created the loop and checked that my vars(i,j) are set properly

--- EasyProblems/validParenthesis/solution.js
+++ EasyProblems/validParenthesis/solution.js
@@ -6,3 +6,21 @@ const valid = {
     '[' : ']'
 }
 
+
+const test1 = "()[]{}";
+const test2 = "()";
+const test3 = '(]'
+
+
+function isValid(s) {
+
+    for(let i = 0; i <= s.length - 2; i+=2) {
+        let j = i + 1;
+        
+    
+    }
+
+}
+
+
+isValid(test3);
